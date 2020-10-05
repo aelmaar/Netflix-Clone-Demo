fc1e3fb1f35aae284a452dec3a0784ec39b3d18b
Anouar <anoirelmaaroufi@gmail.com>
2020-10-05 08:55:48 +0100

    modify the title and delete some unused dependecies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect,useRef} from 'react';
+import React,{useState,useEffect} from 'react';
 import requests from './requests'
 import Navbar from './Navbar'
 import Banner from './Banner'
